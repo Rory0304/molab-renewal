0cf6df51930344d4a7e86e17f0e8315b1153d08f
Rory <mitty0304@naver.com>
2023-09-08 17:11:47 +0900

    chore(next.config.js0: fix hostname

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "xmkqclhjjvshhgohnwrg.supabase.co",
+        hostname: "vqfgzrqmqfbuwypfrvop.supabase.co",
       },
     ],
   },
