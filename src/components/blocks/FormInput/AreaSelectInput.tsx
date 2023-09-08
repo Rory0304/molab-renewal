e721d6cb2c21180be05067653fa456668f4340da
Rory <mitty0304@naver.com>
2023-09-08 17:26:31 +0900

    feat(AreaSelectInput): add area select input props

--- src/components/blocks/FormInput/AreaSelectInput.tsx
+++ src/components/blocks/FormInput/AreaSelectInput.tsx
@@ -3,23 +3,33 @@ import { UseFormRegisterReturn } from "react-hook-form";
 import { KOREA_ADMNISTRATIVE_DISTRICT } from "src/constants/area";
 
 interface AreaSelectInputProps {
-  selectedSido: string;
+  selectedSido?: string;
+  selectedSiGunGu?: string;
   siDoSelectInputRegister?: UseFormRegisterReturn;
   siGunGuSelectInputRegister?: UseFormRegisterReturn;
+  label?: boolean;
+  required?: boolean;
   siDoError?: boolean;
   siGunGuError?: boolean;
   SiDoErrorMessage?: React.ReactElement;
   SiGunGuErrorMessage?: React.ReactElement;
+  onSidoChange?: (value: string) => void;
+  onSiGunGuChange?: (value: string) => void;
 }
 
 const AreaSelectInput: React.FC<AreaSelectInputProps> = ({
   selectedSido,
+  selectedSiGunGu,
   siDoSelectInputRegister,
   siGunGuSelectInputRegister,
+  label = false,
+  required = false,
   siDoError = false,
   siGunGuError = false,
   SiDoErrorMessage,
   SiGunGuErrorMessage,
+  onSidoChange,
+  onSiGunGuChange,
 }) => {
   const sido = Object.keys(KOREA_ADMNISTRATIVE_DISTRICT);
   const [currentSiGunGu, setCurrentSiGunGu] = React.useState<string[]>([]);
@@ -37,20 +47,30 @@ const AreaSelectInput: React.FC<AreaSelectInputProps> = ({
   return (
     <div className="flex flex-row w-full pb-8 form-control">
       <div className="pr-4">
-        <label className="label">
-          <span className="text-lg font-bold label-text after:required-star">
-            시도
-          </span>
-        </label>
+        {label ? (
+          <label className="label">
+            <span
+              className={`text-lg font-bold label-text ${
+                required ? "after:required-star" : null
+              }`}
+            >
+              시도
+            </span>
+          </label>
+        ) : null}
         <select
-          {...siDoSelectInputRegister}
+          value={selectedSido}
+          onChange={(e) =>
+            typeof onSidoChange === "function"
+              ? onSidoChange(e.target.value)
+              : null
+          }
           className={`select select-bordered ${
             siDoError ? "select-error" : ""
           }`}
+          {...siDoSelectInputRegister}
         >
-          <option disabled value="">
-            시도 전체
-          </option>
+          <option value="">시도 전체</option>
           {sido.map((item) => (
             <option key={item} value={item}>
               {item}
@@ -60,20 +80,30 @@ const AreaSelectInput: React.FC<AreaSelectInputProps> = ({
         {React.isValidElement(SiDoErrorMessage) ? SiDoErrorMessage : null}
       </div>
       <div>
-        <label className="label">
-          <span className="text-lg font-bold label-text after:required-star">
-            시군구
-          </span>
-        </label>
+        {label ? (
+          <label className="label">
+            <span
+              className={`text-lg font-bold label-text ${
+                required ? "after:required-star" : null
+              }`}
+            >
+              시군구
+            </span>
+          </label>
+        ) : null}
         <select
-          {...siGunGuSelectInputRegister}
+          value={selectedSiGunGu}
+          onChange={(e) =>
+            typeof onSiGunGuChange === "function"
+              ? onSiGunGuChange(e.target.value)
+              : null
+          }
           className={`select select-bordered ${
             siGunGuError ? "select-error" : ""
           }`}
+          {...siGunGuSelectInputRegister}
         >
-          <option disabled value="">
-            시군구 전체
-          </option>
+          <option value="">시군구 전체</option>
           {currentSiGunGu.map((item) => (
             <option key={item} value={item}>
               {item}
