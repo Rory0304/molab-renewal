93c2dc31bb2f586b55b5ef600773ea942cf2c62e
Rory <mitty0304@naver.com>
2023-09-08 17:21:58 +0900

    feat: add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,28 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>페이지를 찾을 수 없습니다.</p>
+    <div className="flex flex-col items-center justify-center py-20 content-layout">
       <Image
         src={"/not-found.svg"}
         alt="not found image"
-        width={300}
-        height={300}
+        width={250}
+        height={250}
       />
+      <div className="flex flex-col items-center py-10">
+        <h2 className="mb-2 text-3xl font-extrabold">
+          죄송합니다, 페이지를 찾을 수 없습니다.
+        </h2>
+        <p className="mb-8 font-semibold text-center text-gray-400">
+          404 Page not found
+        </p>
+        <Link href="/" className="btn btn-primary btn-md">
+          홈으로 돌아가기
+        </Link>
+      </div>
     </div>
   );
 };
