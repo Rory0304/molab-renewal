bee8aa8f22de3af0bfd6506f6c5dcb60cd463619
Rory <mitty0304@naver.com>
2023-09-08 17:25:26 +0900

    feat(ErrorBox): add error box component

---
+++ src/components/blocks/ErrorBox/ErrorBox.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import ExclamationCircleIcon from "@heroicons/react/24/solid/ExclamationCircleIcon";
+
+interface ErrorBoxProps {
+  title: string;
+  description?: string;
+  onRefetch?: () => void;
+}
+
+const ErrorBox: React.FC<ErrorBoxProps> = ({
+  title,
+  description,
+  onRefetch,
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <ExclamationCircleIcon className="w-8 h-8 mb-2" />
+      <strong className="mb-2 text-lg font-bold text-neutral-600">
+        {title}
+      </strong>
+      {description ? (
+        <p className="mb-2 text-lg font-semibold text-neutral-600">
+          {description}
+        </p>
+      ) : null}
+      {typeof onRefetch ==='function' ? (
+        <button className="btn btn-outline btn-neutral" onClick={onRefetch}>
+          다시 시도
+        </button>
+      ) : null}
+    </div>
+  );
+};
+
+export default ErrorBox;
