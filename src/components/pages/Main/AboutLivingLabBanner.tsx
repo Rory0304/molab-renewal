f0d5ab5fd4b5a732efe6e8ed6b277cb620f372a9
Rory <mitty0304@naver.com>
2023-09-08 17:39:39 +0900

    style(AboutLivingLabBanner): fix background image style

--- src/components/pages/Main/AboutLivingLabBanner.tsx
+++ src/components/pages/Main/AboutLivingLabBanner.tsx
@@ -4,23 +4,14 @@ import Link from "next/link";
 const AboutLivingLabBanner: React.FC = () => {
   return (
     <div className="w-auto rounded-xl">
-      <Link
-        href="/main"
-        style={{
-          overflow: "hidden",
-          display: "block",
-          paddingBottom: "56.72%",
-          background:
-            "url('/point.png') calc(100% - 25px) 100% / 120px 120px no-repeat #172945",
-          borderRadius: "0.75rem",
-          height: 0,
-        }}
-      >
-        <div className="px-8 py-8">
-          <strong className="pt-2 text-lg text-white">
-            {'살아있는 실험실,\n"리빙랩"을 알아보세요'}
-          </strong>
-          <p className="text-slate-400">#시민 #도시문제해결</p>
+      <Link href="/main">
+        <div className="bg-banner bg-[length:130px_130px] bg-no-repeat bg-right-bottom bg-[#172945] overflow-hidden block pb-[180px] lg:pb-[56.72%] rounded-xl h-0">
+          <div className="px-8 py-8">
+            <strong className="pt-2 text-lg text-white">
+              {'살아있는 실험실,\n"리빙랩"을 알아보세요'}
+            </strong>
+            <p className="text-slate-400">#시민 #도시문제해결</p>
+          </div>
         </div>
       </Link>
     </div>
