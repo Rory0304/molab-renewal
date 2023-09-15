6c690b2a977c09b44a1a9d6dacf9fe8da1140e9c
Rory <mitty0304@naver.com>
2023-09-15 15:52:24 +0900

    feat(page): prevent mobile mode and fix current step slug

--- src/app/project/[...slug]/page.tsx
+++ src/app/project/[...slug]/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { useMedia } from "react-use";
 import {
   ProposeProjectDetailEditor,
   ProposeProjectInformationEditor,
@@ -9,9 +13,15 @@ import {
 } from "src/components/pages";
 
 import { ProduceStepType } from "src/types/common";
+import ExclamationCircleIcon from "@heroicons/react/24/solid/ExclamationCircleIcon";
+import { PROJETC_DETAIL_STEP } from "src/constants/projectSteps";
+import { useRouter } from "next/navigation";
 
 const ProposeDetailPage = ({ params }: { params: { slug: string[] } }) => {
-  const currentStep = params.slug?.[1];
+  const router = useRouter();
+  const [projectId, currentStep, currentStepItem] = params.slug;
+
+  const isTabletOrSmaller = useMedia("(max-width: 768px)");
 
   const renderProposeFormSection = (type: ProduceStepType) => {
     switch (type) {
@@ -21,19 +31,34 @@ const ProposeDetailPage = ({ params }: { params: { slug: string[] } }) => {
       case "participation":
         return <ProposeProjectParticipationEditor />;
 
-      case "introduce":
-        return <ProposeProjectDetailEditor />;
+      case "detail":
+        const step = params.slug?.[2] || PROJETC_DETAIL_STEP[0].key;
+        return <ProposeProjectDetailEditor currentStep={step} />;
 
       default:
+        router.push(`/project/${projectId}/base`);
         return null;
     }
   };
 
+  if (isTabletOrSmaller) {
+    return (
+      <div className="flex flex-col items-center justify-center w-screen h-screen content-layout">
+        <ExclamationCircleIcon width={32} height={32} />
+        <strong className="mb-3 text-center">
+          데스크탑 환경에서 이용해주세요.
+        </strong>
+        <Link href="/" className="btn btn-primary ">
+          홈으로
+        </Link>
+      </div>
+    );
+  }
   return (
     <>
       <ProjectEidtorHeader />
       <div className="relative flex w-full overflow-auto h-[calc(100vh-var(--sticky-header-height))]">
-        <ProposeProjectToc currentStep={currentStep} />
+        <ProposeProjectToc currentStep={currentStepItem || currentStep} />
         <div className="w-full">
           <form className="relative flex flex-col p-10">
             {renderProposeFormSection(currentStep as ProduceStepType)}
