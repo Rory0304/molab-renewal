cbd82bbb0d638942eed10ce6b366f25b2f2610e6
Rory <mitty0304@naver.com>
2023-09-15 15:53:04 +0900

    style(OverlayLoading): add full height of editor

--- src/components/blocks/OverlayLoading/OverlayLoading.tsx
+++ src/components/blocks/OverlayLoading/OverlayLoading.tsx
@@ -10,7 +10,7 @@ const OverlayLoading: React.FC<OverlayLoadingProps> = ({
   description,
 }) => {
   return (
-    <div className="absolute top-0 bottom-0 left-0 right-0 z-50 flex flex-col items-center justify-center w-full h-screen overflow-hidden bg-gray-200 opacity-80">
+    <div className="absolute top-0 bottom-0 left-0 right-0 z-50 flex flex-col items-center justify-center w-full h-full overflow-hidden bg-gray-200 opacity-80">
       <span className="loading loading-dots loading-lg text-primary"></span>
       {title ? (
         <h2 className="text-xl font-semibold text-center">{title}</h2>
