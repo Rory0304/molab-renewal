f963854f6320849fa8a7d87c524fe489f9c55c97
Rory0304 <mitty0304@naver.com>
2023-12-14 15:53:48 +0900

    style(app): add bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import {
 
 const MainPage: React.FC = () => {
   return (
-    <div className="content-layout">
-      <div className="flex flex-col justify-between my-12 lg:flex-row">
+    <div className="bg-gray-50">
+      <div className="content-layout py-12 flex flex-col justify-between lg:flex-row">
         <section className="w-full lg:w-[800px] lg:mb-0 mb-10 lg:mr-8 mr-0">
           <MainBanner />
           <HydrateMainNoticeList />
