d08082be35be45126cfe44ee0e8ce35936514419
Rory0304 <mitty0304@naver.com>
2024-01-17 19:50:25 +0900

    chore(NoticeBanner): fix notice banner text

--- src/components/pages/Notice/NoticeBanner.tsx
+++ src/components/pages/Notice/NoticeBanner.tsx
@@ -18,7 +18,7 @@ const NoticeBanner: React.FC = () => {
       <div className="text-center content-layout w-full">
         <h3 className="pb-2 text-3xl font-bold text-white ">리빙랩 공고</h3>
         <p className="text-white">
-          내가 살고 있는 지역의 리빙랩 프로젝트를 알아보세요
+          내가 살고 있는 지역에서 주최된 리빙랩 프로젝트를 알아보세요
         </p>
       </div>
     </div>
