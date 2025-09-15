e9f9949d494b810a309c983f6e755f765b616da8
Rory0304 <mitty0304@naver.com>
2025-09-16 01:56:57 +0900

    feat: add ReviewRepository and ReviewUseCase for managing reviews functionality

---
+++ src/repositories/review/ReviewRepository.ts
@@ -0,0 +1,98 @@
+import { decamelizeKeys } from 'humps';
+import { MolabApiClient } from 'src/repositories/api/molab/MolabApiClient';
+import type { Row, SupabaseClientType } from 'src/types/supabase';
+import { ImageUseCase } from 'src/useCases/image';
+
+export type ReviewType = Row<'Review'>;
+
+export class ReviewRepository extends MolabApiClient {
+  constructor(client?: SupabaseClientType) {
+    super({ client: client });
+  }
+  /**
+   *
+   */
+  public async fetchReviewById({ uuid }: { uuid: string }) {
+    const { data, error } = await this.client
+      .from('Review')
+      .select('*')
+      .eq('uuid', uuid)
+      .limit(1)
+      .single<Row<'Review'>>();
+
+    if (error) throw Error('fail to fetch review');
+
+    return data;
+  }
+
+  /**
+   *
+   */
+  public async fetchReviewList(props: {
+    select: string;
+    offset: number;
+    pageCount: number;
+    projectId?: string;
+  }) {
+    const { select, offset, pageCount, projectId } = props;
+
+    let query = this.client
+      .from('Review')
+      .select(select)
+      // Filters
+      .range(offset, offset + pageCount);
+
+    if (projectId) {
+      query = query.eq('project_id', projectId);
+    }
+
+    const { data, error } = await query;
+
+    if (error) {
+      throw Error('fail to fetch review list');
+    }
+    return data as Partial<ReviewType>[];
+  }
+
+  /**
+   * Upload Review
+   */
+  public async uploadReview(props: {
+    projectId: string;
+    uuid: string;
+    content: string;
+    userId: string;
+    imageFile?: File;
+  }) {
+    const { projectId, uuid, content, userId, imageFile } = props;
+    const imageUsecase = new ImageUseCase(this.client);
+
+    // upload image
+    const thumbnailFilePath = imageFile
+      ? await imageUsecase.uploadImage(
+          'review_thumbnail',
+          `${uuid}-thumbnail`,
+          imageFile
+        )
+      : '';
+
+    const configuredData = decamelizeKeys({
+      projectId,
+      content,
+      userId,
+      uuid: uuid,
+      thumbnail: thumbnailFilePath,
+    }) as ReviewType;
+
+    const { data, error } = await this.client
+      .from('Review')
+      .insert(configuredData)
+      .returns<Row<'Review'>>();
+
+    if (error) {
+      throw new Error('fail to upload reivew');
+    }
+
+    return data;
+  }
+}
